c5481f1d463ea181eb61e67a3484becc5587b6e8
bazaam <bazaam1@gmail.com>
2016-12-06 08:39:56 -0800

    project cleanup for fresh start with bill

--- Assets/ImportedAssets/RimShader/Shield Scroll.js
+++
@@ -1,9 +0,0 @@
-var scrollX = 5.0*Time.time;
-var scrollY = 5.0*Time.time;
-
-function Update() {
-    var offset : float = Time.time * scrollX;
-    var offset2 : float = Time.time * scrollY;
-    GetComponent.<Renderer>().material.SetTextureOffset ("_ShieldTexture", Vector2(offset, offset2));
-    GetComponent.<Renderer>().material.SetTextureOffset ("_ShieldTex2", Vector2(offset, offset2)*2);
-}
