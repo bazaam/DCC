a5b8ba69aaba960c43874ae12cfe2028faf6e8a3
bazaam <bazaam1@gmail.com>
2016-12-06 10:43:54 -0800

    dungeon generator script

--- Assets/Assets/Scripts/DungeonGenerator.cs
+++ Assets/Assets/Scripts/DungeonGenerator.cs
@@ -1,29 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 
 public class DungeonGenerator : MonoBehaviour
 {
+    List<List<int>> levelMap = new List<List<int>>();
+    DungeonGen.Map dungeonMap = new DungeonGen.Map(100, 120, 750);
 
-    public GameObject Dungeon;
-    public GameObject Player;
-
-
-
-    void Awake()
+    void SpawnDungeon()
     {
-        CreateNewDungeonLevel();
+
+        public GameObject wall;
+        public GameObject floor;
+
+        
+        
+
+        levelMap = dungeonMap.IntMap;
+        int x = 0;
+        int y = 0;
+
+        foreach (List<int> subList in levelMap) 
+        {
+	        foreach (int tileType in subList) 
+            {
+
+                InstantiateDungeonTile(x, y, tileType);
+		        ++y;
+	        }
+
+	        ++x;
+        }
     }
-    void Start()
+
+    void InstantiateDungeonTile(int x, int y, int tileType)
     {
-
-
-
-    }
-
-    public void CreateNewDungeonLevel()
-    {
-        Instantiate(Dungeon);
+        if (tileType == 0)
+        {
+            GameObject newTile = GameObject.Instantiate(floor, Vector3(x, y, 0), Quaternion.identity);
+        }
+        else if (tileType == 1)
+        {
+            Instantiate(wall, Vector3(x, y, 0), Quaternion.identity);
     }
 }
+
+
+
+
+
+
+
