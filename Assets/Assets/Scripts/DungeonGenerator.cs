9e4db57b7fb27124568e9eaf92a9d4ec45c5bc39
bazaam <bazaam1@gmail.com>
2016-12-06 10:56:56 -0800

    updating dungeongenerator to work correctly

--- Assets/Assets/Scripts/DungeonGenerator.cs
+++ Assets/Assets/Scripts/DungeonGenerator.cs
@@ -8,30 +8,26 @@ public class DungeonGenerator : MonoBehaviour
 {
     List<List<int>> levelMap = new List<List<int>>();
     DungeonGen.Map dungeonMap = new DungeonGen.Map(100, 120, 750);
+    public GameObject wall;
+    public GameObject floor;
 
-    void SpawnDungeon()
+    public void SpawnDungeon()
     {
-
-        public GameObject wall;
-        public GameObject floor;
-
-        
-        
 
         levelMap = dungeonMap.IntMap;
         int x = 0;
         int y = 0;
 
-        foreach (List<int> subList in levelMap) 
+        foreach (List<int> subList in levelMap)
         {
-	        foreach (int tileType in subList) 
+            foreach (int tileType in subList)
             {
 
                 InstantiateDungeonTile(x, y, tileType);
-		        ++y;
-	        }
+                ++y;
+            }
 
-	        ++x;
+            ++x;
         }
     }
 
@@ -39,11 +35,12 @@ public class DungeonGenerator : MonoBehaviour
     {
         if (tileType == 0)
         {
-            GameObject newTile = GameObject.Instantiate(floor, Vector3(x, y, 0), Quaternion.identity);
+            Instantiate(floor, new Vector3(x, y, 0), Quaternion.identity);
         }
         else if (tileType == 1)
         {
-            Instantiate(wall, Vector3(x, y, 0), Quaternion.identity);
+            Instantiate(wall, new Vector3(x, y, 0), Quaternion.identity);
+        }
     }
 }
 
