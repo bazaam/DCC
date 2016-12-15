0e1ca64086c38fa8bb98a28803b4519c55c98d29
bazaam <bazaam1@gmail.com>
2016-12-15 13:43:34 -0800

    Fun with lasers. Also updated firing mechanics.

---
+++ Assets/Assets/Scripts/BasicEnemyAttackBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BasicEnemyAttackBehavior : MonoBehaviour {
+
+    public float moveSpeed;
+
+    GameObject player;
+    GameObject target;
+    GameObject[] baseDestructibles;
+    
+    Transform GetClosestObject (GameObject[] potentialTargets) {
+        
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach (GameObject potentialTarget in potentialTargets)
+        {
+            Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget.transform;
+            }
+        }
+
+        return bestTarget;
+    }
+
+    void FindNewTarget()
+    {
+        target = GetClosestObject(baseDestructibles).gameObject;
+        transform.LookAt(target.transform);
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        baseDestructibles = GameObject.FindGameObjectsWithTag("BaseDestructible");
+        FindNewTarget();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target.transform);
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Assets/Scripts/WeaponFireController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFireController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward, Color.green);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100)) {
+                if (hit.collider.gameObject.tag == "Enemy")
+                {
+                    hit.collider.SendMessageUpwards("ApplyDamage", 100f);
+                }
+            }
+        }
+    }
+}
