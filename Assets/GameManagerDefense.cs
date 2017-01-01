5a8469771b5b1fcd2fb7aaa64d49e06545ba460f
bazaam <bazaam1@gmail.com>
2017-01-01 11:36:52 -0800

    pause menu properly responds to "escape" key and locks out player controls. Audio buttons don't work yet, probably will require refactor

---
+++ Assets/GameManagerDefense.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerDefense : MonoBehaviour
+{
+
+    bool isPaused = true;
+
+    public GameObject player;
+    public GameObject pauseMenu;
+
+    public void TogglePauseMenu()
+    {
+        if (isPaused)
+        {
+            pauseMenu.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+            isPaused = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            player.GetComponentInChildren<LaserFire>().enabled = true;
+        }
+
+        else
+        {
+            pauseMenu.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0.0f;
+            isPaused = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            player.GetComponentInChildren<LaserFire>().enabled = false;
+
+        }
+    }
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        TogglePauseMenu();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameManagerDefense gameManager;
+    public SoundManager soundManager;
+
+    private Slider volumeSlider;
+
+    void Start()
+    {
+        volumeSlider = GameObject.Find("VolumeSlider").GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+            gameManager.TogglePauseMenu();
+    }
+
+    public void MusicSliderUpdate(float val)
+    {
+        soundManager.SetVolume(val);
+    }
+
+    public void MusicToggle(bool val)
+    {
+        volumeSlider.interactable = val;
+        soundManager.SetVolume(val ? volumeSlider.value : 0f);
+    }
+}
